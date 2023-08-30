a932162e07931b16fb3497bf2d11de66f7f085bd
Faqu22 <facundoalvarez415@gmail.com>
2023-08-29 23:00:58 -0300

    Task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,9 +4,9 @@ Task 1
 """
 import asyncio
 import random
+from typing import List
 
-
-async def wait_n(n, max_delay: int = 10) -> list:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Asynchronous coroutine that waits for a random delay."""
     wait_random = __import__('0-basic_async_syntax').wait_random
     delays = []
