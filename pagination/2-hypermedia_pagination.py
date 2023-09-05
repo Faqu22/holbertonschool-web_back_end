7383231ef8b66643a5b6a61e59481e410892889f
Faqu22 <facundoalvarez415@gmail.com>
2023-09-05 20:03:44 -0300

    tas 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -66,7 +66,7 @@ class Server:
             hyper["data"] = self.get_page(page, page_size)
             if not hyper["data"]:
                 hyper["page_size"] = 0
-            hyper["next_page"] = None if page + 2 > len(f_read) else page + 1
+            hyper["next_page"] = None if page * page_size > len(f_read) else page + 1
             hyper["prev_page"] = None if page < 2 else page - 1
             hyper["total_pages"] = int(len(f_read) / page_size)
 
