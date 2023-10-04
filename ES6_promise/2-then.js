e26e2000c01bb5696f2dce9cf232ac1b0bcf04c5
Faqu22 <facundoalvarez415@gmail.com>
2023-10-04 10:17:37 -0300

    Task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((success) => ({ status: 200, body: success })).catch(() => Error()).finally(() => {
+  promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }
